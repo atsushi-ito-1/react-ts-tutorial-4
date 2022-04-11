8f3ee9e4a1c5b5e372f7ab220427306344e85f77
atsushi-ito-1 <atsushi.ito@jp.ricoh.com>
2022-04-11 16:40:55 +0900

    pwa.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { Game } from "./pages/Game";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const App = () => {
   return (
@@ -12,3 +14,6 @@ const App = () => {
 
 const container = document.getElementById("root");
 if (container) createRoot(container).render(<App />);
+
+serviceWorkerRegistration.register();
+reportWebVitals();
